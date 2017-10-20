b149c0eb0dc57c3886a32a67aafc0984d1cce377
James Brown <jbrown97@mgh.harvard.edu>
2017-10-20 18:06:09 -0400

    Adapting to code to work with theano/tensorflow

---
+++ qtim_OA/__init__.py
@@ -0,0 +1,12 @@
+import os
+from keras import backend as K
+
+if os.name == 'nt':
+    os.environ['KERAS_BACKEND'] = 'theano'
+    K.set_image_data_format('channels_first')
+else:
+    os.environ['KERAS_BACKEND'] = 'tensorflow'
+    K.set_image_data_format('channels_last')
+
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
